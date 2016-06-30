e3e886c3a3069b1e36b5ba9471127e4afa2fd1a4
jeffdaley <jdaleydesign@gmail.com>
2016-06-30 16:49:43 -0500

    Revert "initial commit"
    This reverts commit 57b98d2d7161b9f2fc24c79669a8201014fb5c5a.

--- README.md
+++
@@ -1,7 +0,0 @@
-## To Stage
-
-Run `middleman`.
-
-## To Deploy
-
-Run `rake deploy` (we made a Rake task that bundles `middleman build` and `middleman publish`)
--- source/javascripts/all.js
+++
@@ -1,6 +0,0 @@
-//= require "jquery.js"
-//= require "expand.js"
-//= require "foundation.js"
-//= require "magnificent-popup.js"
-//= require "app.js"
-//= require "retina.js"
