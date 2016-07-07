f0b6a988e44bcb29386e017ba8cfe1cc87524ae3
jeffdaley <jdaleydesign@gmail.com>
2016-07-07 16:32:59 -0500

    More updates to Women page
    + Adds function to expand the bio panel if #name is in the URL
    + Style adjustments
    + Updated data

--- source/javascripts/expand.js
+++ source/javascripts/expand.js
@@ -1,12 +1,17 @@
 
 $(document).ready(function() {
-  $('.woman a').click(function(event) {
+      if(window.location.hash) {
+        jQuery(location.hash).addClass('active').removeClass('inactive');
+      }
+  $('.woman > a').click(function(event) {
     event.preventDefault();
     if ($(this).parent('.woman').hasClass('active')) {
       $(this).parent('.woman').removeClass('active');
+      $(this).parent('.woman').siblings().removeClass('inactive');
     } else {
       $(this).parent('.woman').siblings().removeClass('active');
-      $(this).parent('.woman').removeClass('active').addClass('active');
+      $(this).parent('.woman').removeClass('active').removeClass('inactive').addClass('active');
+      $(this).parent('.woman').siblings().removeClass('inactive').addClass('inactive');
     }
     $('html,body').animate({
       scrollTop: $(this).offset().top}
