b9ea93edee5a328c3ff80a10853bd9ec0adf5fae
wifelette <leahsilber@gmail.com>
2016-08-18 13:07:41 -0700

    Automated commit at 2016-08-18 20:07:41 UTC by middleman-deploy 1.0.0

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 
 css_dir = "css" # by Compass.app 
-sass_dir = "sass" # by Compass.app 
+sass_dir = "scss" # by Compass.app
 images_dir = "images" # by Compass.app 
 output_style = :expanded # by Compass.app 
 relative_assets = false # by Compass.app 
